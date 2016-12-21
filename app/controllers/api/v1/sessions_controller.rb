439d86ba2ac75ffaee7233196f5954007d40edb1
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2016-12-22 00:30:25 +0200

    removed devise sign_in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -12,7 +12,6 @@ module Api
       def create
         user = User.find_by(email: params[:email])
         raise InvalidAPIRequest.new('Invalid username or password', 401) unless valid_sign_in?(user)
-        sign_in user
         user.generate_authentication_token!
         user.save(validate: false)
         render json: { success: true, data: user.as_json(only: :auth_token) }, status: 200
@@ -24,10 +23,6 @@ module Api
         user.generate_authentication_token!
         user.save(validate: false)
         head 204
-      end
-
-      def sign_in(resource)
-        Devise::Mapping.find_scope!(resource)
       end
 
       def valid_sign_in?(user)
