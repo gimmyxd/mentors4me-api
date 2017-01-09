10be696092255a2cd2a652edcb8080c008544720
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2017-01-10 00:25:03 +0200

    fixed context permision

--- app/abilities/organization_factory.rb
+++ app/abilities/organization_factory.rb
@@ -4,5 +4,6 @@ class OrganizationFactory < Ability
     can [:create], User
     can :update, Organization, organization: user.organization
     can [:update, :password], User, id: user.id
+    can :manage, Context, organization_id: user.id
   end
 end
