a9fc86c592cd0b1166f139b0d424068d53338ff0
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2017-01-11 23:03:38 +0200

    updated email link

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -1,6 +1,6 @@
 class InvitationsMailer < ApplicationMailer
   def send_invitation(email, auth_token)
-    url = "#{ENV['FRONTEND_URL']}/#{auth_token}"
+    url = "#{ENV['FRONTEND_URL']}/#!/mentors/register/#{auth_token}"
 
     mail = Mail.new
     mail.from = Email.new(email: ENV['EMAIL_FROM'])
